6021e96018aa84060e3748939238a7ac6958f70d
Kavi <kavipanneer145@gmail.com>
2026-05-21 22:05:53 +0530

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,5 +1,5 @@
 // Base Configuration URL - Swap with your live render/railway URL for deployment!
-const API_BASE_URL = "http://127.0.0.1:8000"; 
+ const API_BASE_URL = "https://task-manager-app-hyg3.onrender.com";
 
 // Authentication Router Hook
 async function handleAuth(endpoint) {
